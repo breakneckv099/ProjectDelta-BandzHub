950dff264bb324f57ee5380cb51a08fafe6546f0
breakneckv099 <vexormodz@gmail.com>
2025-08-21 13:34:36 -0400

    Update Project Delta Private V2.lua

--- Project Delta Private V2.lua
+++ Project Delta Private V2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://cdn.authguard.org/virtual-file/dd7b2d569e13440a8b17b80dbb8e1b58'))()
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/95b030692b3241269f58690f650a1b3c"))()
